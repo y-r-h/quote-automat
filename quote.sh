231dc793c3e03b5f0188519db80b47900c584a93
y-r-h <yukirea.hamano@gmail.com>
2021-05-10 14:46:27 +0000

    changed crontab time to 12:00 everyday

--- quote.sh
+++ quote.sh
@@ -1,3 +1,5 @@
 date >> ~/quote-automat/quote.txt
 curl https://www.brainyquote.com/quote_of_the_day | egrep "\"&quot;.*\"" | sed "s/<meta.*content=\"&quot;//g" | sed "s/&quot;//g" | sed "s/\">//g" >> quote.txt
 echo " " >> ~/quote-automat/quote.txt
+git add -A
+git commit -m "added quote of the day"
