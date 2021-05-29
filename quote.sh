0e6ea3f94a2c03be1143d9aaf0004d746eda15af
y-r-h <yukirea.hamano@gmail.com>
2021-05-29 04:24:49 +0000

    added quote of the day

--- quote.sh
+++ quote.sh
@@ -1,5 +1,5 @@
 date >> ~/quote-automat/quote.txt
-curl https://www.brainyquote.com/quote_of_the_day | egrep "\"&quot;.*\"" | sed "s/<meta.*content=\"&quot;//g" | sed "s/&quot;//g" | sed "s/\">//g" >> quote.txt
+curl https://www.brainyquote.com/quote_of_the_day | egrep "\"&quot;.*\"" | sed "s/<meta.*content=\"&quot;//g" | sed "s/&quot;//g" | sed "s/\">//g" >> ~/quote-automat/quote.txt
 echo " " >> ~/quote-automat/quote.txt
 git add -A
 git commit -m "added quote of the day"
